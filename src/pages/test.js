4bc66cd9cb3a174ff363d5b2d952100f70dc805a
Utkarsh Singh <utkarsh.singh27800@gmail.com>
2023-06-17 10:54:28 +0530

    deploy final

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from 'react'
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  } from "@fortawesome/free-solid-svg-icons";
-import {  } from "@fortawesome/free-regular-svg-icons";
+import React from 'react'
 
 function Test2() {
 
     return (
         <>
-        <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
         </>
     )
 }
